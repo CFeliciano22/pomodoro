62a23ede930f53d4694f7e26cf9be5633c44618d
Cesar Feliciano <cesarfeliciano1@aol.com>
2022-06-04 01:19:08 -0400

    imported modal from bootstrap

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import './App.css';
 import { CircularProgressbar } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
-
+import Modal from './Components/Modal/Modal';
 
 function App() {
   return (
     <main className="App" style={{ width: 200, height: 200 }}>
         <CircularProgressbar value={60} text={`${60}%`} />
-        
+        {/* <Modal /> */}
     </main>
   );
 }
