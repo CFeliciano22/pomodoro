ee6da256851b2e6bfed325ecf886856654b0f1b3
Cesar Feliciano <cesarfeliciano1@aol.com>
2022-06-04 01:03:49 -0400

    added more components to the folder and installed the circular progress bar to use as timer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import './App.css';
+import { CircularProgressbar } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
+
 
 function App() {
   return (
-    <div className="App">
-     pomodoro clock
-    </div>
+    <main className="App" style={{ width: 200, height: 200 }}>
+        <CircularProgressbar value={60} text={`${60}%`} />
+        
+    </main>
   );
 }
+
 
 export default App;
